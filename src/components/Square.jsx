cc00b68fbb226031d50601c6eb503f34ae675d6c
Nicholas Stobie <npstobie@gmail.com>
2017-11-01 20:06:25 +0800

    changed style color to style background-color (want to change the color of the square not the text within the square)

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 export default function Square(props){
 	return (
-		<div
-			className="square"
-			style={{color: props.color}}
-		>
+		<div className="square" style={{'background-color': props.color}}>
 			{props.value}
 		</div>
 	)
