b60fccdf1730c4d3ba10b78d6f2c516ae8a05d60
Nicholas Stobie <npstobie@gmail.com>
2017-11-05 12:56:46 +0800

    created StepBack component which allows users to undo a move and set the state back one

---
+++ src/components/StepBack.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function StepBack(props){
+	return (
+    <button onClick={props.onClick} className="col-md-12 col-sm-12 col-xs-12 col-lg-12 btn btn-default" disabled={props.disabled}>
+      Reverse Last Move
+    </button>
+	)
+}
