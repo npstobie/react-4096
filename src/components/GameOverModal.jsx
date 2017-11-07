aeeb4686cc03f19ae1d5979e31b8d6922c0d94e5
Nicholas Stobie <npstobie@gmail.com>
2017-11-07 19:31:05 +0800

    added a modal component that appears when the player loses

---
+++ src/components/GameOverModal.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function GameOverModal(props){
+	return (
+		<div className="modal fade" id="myModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div className="modal-dialog" role="document">
+		    <div className="modal-content">
+		      <div className="modal-header">
+		        <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <h4 className="modalText" id="myModalLabel">Game Over!</h4>
+		      </div>
+		      <div className="modal-body modalText">
+		        Your score: {props.score}
+		      </div>
+		      <div className="modal-footer">
+		        <button onClick={props.onStepBackClick} type="button" className="btn btn-default modalText modalBtns" data-dismiss="modal">Close</button>
+		        <button onClick={props.onNewGameClick} type="button" className="btn btn-default modalText modalBtns"  data-dismiss="modal">New Game</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	)
+}
