296400e55579c0db0fc1f0f7022c6a55eabade4c
Nicholas Stobie <npstobie@gmail.com>
2017-12-04 14:35:38 -0800

    updated button text

--- client/src/components/StepBack.jsx
+++ client/src/components/StepBack.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function StepBack(props){
 	return (
-    <button onClick={props.onClick} className="col-md-12 col-sm-12 col-xs-12 col-lg-12 btn btn-default stepback" disabled={props.disabled}>
-      Reverse Last Move
+    <button onClick={props.onClick} className="col-sm-12 col-xs-12 col-lg-8 col-lg-offset-2 btn btn-default stepback" disabled={props.disabled}>
+      Reverse Move
     </button>
 	)
 }
