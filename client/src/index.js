a085230fa1bb3c8c0eaab44ad783c34ad0e6a141
Nicholas Stobie <npstobie@gmail.com>
2017-12-11 14:06:02 -0800

    removed react routher as it is not needed or being used

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory } from 'react-router';
-import Routes from './routes';
-import './index.css';
 import Game from './components/Game';
+import './index.css';
 
 ReactDOM.render(
-  <Routes history={browserHistory} />,
+	<Game />,
   document.getElementById('root')
 );
